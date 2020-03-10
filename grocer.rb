6ee9dcbaf53873b81cc741d76551627bd783063b
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-10 19:36:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -62,10 +62,9 @@ def apply_coupons(cart, coupons)
   while index < coupons.count do
     coupon = coupons[index]
     item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
-    item_is_in_basket = !!item_with_coupon
     count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
 
-    if item_is_in_basket and count_is_big_enough_to_apply
+    if item_with_coupon and count_is_big_enough_to_apply
       apply_coupon_to_cart(item_with_coupon, coupon, cart)
     end
     index += 1
