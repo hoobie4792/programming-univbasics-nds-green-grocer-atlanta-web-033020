b9518b793251d5d0769ae7a980ba805427e1fcbd
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-10 19:48:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,6 +77,7 @@ def apply_clearance(cart)
   while index < cart.length do
     if cart[index][:clearance]
       cart[index][:cost] = (0.8 * cart[index][:cost]).round(2)
+    end
     index += 1
   end
   
